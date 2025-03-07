417676da707dc7770aae78e4edea9f248b4e5a7b
carmenvelazq <carmen.pb1005@gmail.com>
2025-03-06 21:36:42 -0800

    Delete request to delete one user

--- backend/server.py
+++ backend/server.py
@@ -7,12 +7,27 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client["packup"]
 users = db["users"]
 
-
 # Example route
 @app.route("/members")
 def members():
     return {"members": ["Member1", "Member2", "Member3"]}
 
+# create new user route
+@app.route("/users", methods=["POST"])
+def create_user():
+    data = request.get_json()
+
+    users.insert_one({
+        "username": data.get("username"),
+        "age": data.get("age")
+    })
+
+    return jsonify({"username": data.get("username")}), 201
+
+@app.route("/users/<username>",methods=["DELETE"])
+def delete(username):
+    users.delete_one({"username": username})
+    return jsonify({"message": f"Deleted {username}"}), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
