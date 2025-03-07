a4ec38da2f3b527a646fb4305c3e54618e9845d1
connorw72 <connorwang2@gmail.com>
2025-03-07 11:31:10 -0800

    delete route

--- backend/server.py
+++ backend/server.py
@@ -16,17 +16,6 @@ users = db["users"]
 def members():
     return {"members": ["Member1", "Member2", "Member3"]}
 
-# create new user route
-@app.route("/users", methods=["POST"])
-def create_user():
-    data = request.get_json()
-
-    users.insert_one({
-        "username": data.get("username"),
-        "age": data.get("age")
-    })
-
-    return jsonify({"username": data.get("username")}), 201
 
 if __name__ == "__main__":
     app.run(debug=True)
