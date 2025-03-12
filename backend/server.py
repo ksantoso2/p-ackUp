854b7d81c88280d4bf71b5ed94413e0ae1bffd7c
el0810 <ellieliann@gmail.com>
2025-03-12 15:18:34 -0700

    creating user backend route

--- backend/server.py
+++ backend/server.py
@@ -13,6 +13,15 @@ users = db["users"]
 def members():
     return {"members": ["Member1", "Member2", "Member3"]}
 
+@app.route("/users", methods=["POST"])
+def create_user():
+    data = request.get_json()
+    username = data["username"]
+    age = data["age"]
+
+    users.insert_one({"username": username, "age": age})
+
+    return jsonify({"message": "User created!", "username": username, "age": age})
 
 if __name__ == "__main__":
     app.run(debug=True)
