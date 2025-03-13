a79da6370e96fc49269e00139c1c9b04de9a82ba
Sarah Yandell <syandell@g.hmc.edu>
2025-03-12 23:51:34 -0700

    got chat page running and env file Co-authored-by: carmenvelazq <carmenvelazq@users.noreply.github.com>

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,18 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
-from flask_cors import CORS
+from flask_cors import CORS     #pip install flask-cors
 from google import genai        #pip install google-genai
+from dotenv import load_dotenv  # pip install python-dotenv
+import os
+
 
 app = Flask(__name__)
-client = genai.Client(api_key="AIzaSyBCFrOAyjn4JHAdJkMylalPl58bSoPr890")    #UPDATE API KEY FOR OTHER USERS
+
+load_dotenv()
+GEMINI_API_KEY = os.getenv("S_GEMINI_API_KEY")
+
+#variable name is "sensitive"? DO NOT CHANGE 
+gemini_client = genai.Client(api_key=GEMINI_API_KEY)    #UPDATE API KEY FOR OTHER USERS
 
 CORS(app)
 
@@ -25,12 +33,21 @@ def delete(username):
 # API route
 @app.route("/gemini", methods = ['POST'])
 def gemini():
-    user_input = request.json.get('user_input')     #UPDATE TO CORRECT INPUT VARIABLE
-    response = client.models.generate_content(
+    data = request.get_json()
+    print("Received data:", data)  
+
+    user_input = data.get("user_input", "").strip()
+    if not user_input:
+        return jsonify({"error": "No input provided"}), 400
+
+    
+    response = gemini_client.models.generate_content(
     model="gemini-2.0-flash",
-    contents="user_input",
+    contents = user_input,
     )
+    print("Response from Gemini:", response.text) 
     return jsonify({"response": response.text}), 200
+
     
 
 if __name__ == "__main__":
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 import CreateUser from './welcome/CreateUser';
 import React from 'react';
+import Chat from './Chat';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";  //if error run: npm install react-router-dom
+
+// function App() {
+//   return (
+//     <CreateUser />
+//   );
+// } 
 
 function App() {
   return (
-    <CreateUser />
+    <Router>
+      <Routes>
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </Router>
   );
 }
 
