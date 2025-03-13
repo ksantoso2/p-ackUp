a033fa80e6cac44693940d94f11ef6af6f6c6931
connorw72 <connorwang2@gmail.com>
2025-03-12 22:19:45 -0700

    Your commit message

--- frontend/src/welcome/Signup.js
+++ frontend/src/welcome/Signup.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Signup = () => {
   const [username, setUsername] = useState('');
   const [age, setAge] = useState('');
   const [message, setMessage] = useState('');
+  const navigate = useNavigate();
 
   const handleUser = async () => {
     await fetch ('http://127.0.0.1:5000/users', {
@@ -15,24 +17,26 @@ const Signup = () => {
     });
 
     setMessage (`User Created!`); // success message
+    navigate('/chat'); // redirect
   }
 
   return (
-    <div style = {{alignItems: "center"}}>
+    <div style = {{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh"}}>
       <input 
         type="text" 
         placeholder="Username" 
         onChange={(e) => setUsername(e.target.value)}
+        style = {{marginBottom: "15px", padding: "8px", width: "210px"}}
       />
-      <br></br>
       <input 
         type="number" 
         placeholder="Age" 
         onChange={(e) => setAge(e.target.value)}
+        style = {{marginBottom: "15px", padding: "8px", width: "210px"}}
       />
-      <br></br>
       <button 
         onClick={handleUser}
+        style= {{padding: "8px 15px"}}
       >
         Create User
       </button>
