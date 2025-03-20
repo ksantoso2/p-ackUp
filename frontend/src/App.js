ea8169dd71ca77fd46ed0fc69fc7921343140b98
Marilyn Ma <marilyn.m5128@gmail.com>
2025-03-20 15:27:21 -0700

    Login page creation, still need debugging

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
-import CreateUser from './welcome/CreateUser';
+import Login from './Login';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import React from 'react';
 
 function App() {
   return (
-    <CreateUser />
+    <Router> {/* Router should wrap everything */}
+      <Routes> {/* Contains all routes */}
+        <Route path="/login" element={<Login />} /> {/* Login Page */}
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+
