93e8652e19804e06e4dece90bfe76f02fbe52464
Sarah Yandell <syandell@g.hmc.edu>
2025-04-20 15:01:26 -0700

    Co-authored-by: Marilyn Ma <marilyn.m5128@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,19 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import React from 'react';
 import Chat from './Chat';
 import Trip from './Trip';
+import TripList from './TripList';
+
 
 function App() {
   return (
     <Router>
       <Routes>
+      <Route path="/Chat" element={<Chat />} />
         <Route path="/Trip" element={<Trip />} />
+        <Route path="/TripList" element={<TripList />} />
       </Routes>
     </Router>
   );
 }
-
 
 export default App;
