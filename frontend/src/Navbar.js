6b2b02c69a9a82c3c450db116b83da84cb6d7508
Natalie <nataliemholert@gmail.com>
2025-05-01 01:12:25 -0700

    edited nav bar buttons to preserve username in the url

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,4 +1,4 @@
-import { useParams, NavLink } from "react-router-dom";
+import { useParams, NavLink, useLocation } from "react-router-dom";
 import "./Navbar.css";
 import Chat from './Chat';
 import TripList from './TripList';
@@ -7,10 +7,14 @@ import ChatboxImg from '../src/assets/chaticon.svg';
 
 
 const Navbar = () => {
-  const { username } = useParams();
+  const location = useLocation();
+  // Getting the username from the current url
+  const match = location.pathname.match(/\/(?:triplist|chat|trip)\/([^/]+)/);
+  const username = match ? match[1] : null;
+
   return (
     <nav className="navbar">
-      <NavLink to={`/trips/${username}`}  className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+      <NavLink to={`/triplist/${username}`} className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
         <span className="icon"><img src={PlaneImg} alt="plane" className="plane-img" /></span> Trips
       </NavLink>
       <NavLink to={`/chat/${username}`} className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
